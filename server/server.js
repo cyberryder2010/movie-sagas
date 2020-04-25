63b3d36830ea67ee57feedd72a2bb0a6609afddb
Pamela Anderson <cyberryder2010@gmail.com>
2020-04-25 14:00:04 -0500

    add routes and server connection for routes

--- server/server.js
+++ server/server.js
@@ -1,16 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const port = process.env.PORT || 5000;
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
-app.use(express.static('build'));
+app.use(express.static("build"));
 
 /** ---------- ROUTES ---------- **/
-
+app.use("/movies", moviesRouter);
+app.use("/genres", genresRouter);
 
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {
-    console.log('Listening on port: ', port);
+  console.log("Listening on port: ", port);
 });

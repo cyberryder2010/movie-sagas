823f07060ac465c7fcfe0a4dc19e1e1d20aa945d
Pamela Anderson <cyberryder2010@gmail.com>
2020-04-26 18:22:08 -0500

    genres router added

--- server/routes/genres.router.js
+++ server/routes/genres.router.js
@@ -16,4 +16,21 @@ genresRouter.get("/", (req, res) => {
     });
 });
 
+genresRouter.get("/movie/:id", (req, res) => {
+  const queryString = `SELECT * FROM "movies"
+  JOIN "movies_genres" ON "movies".id = "movies_genres".movies_id
+  JOIN "genres" ON  "genres".id = "movies_genres".genres_id
+  WHERE "movies".id = $1`;
+
+  pool
+    .query(queryString, [req.params.id])
+    .then((response) => {
+      res.send(response.rows);
+    })
+    .catch((err) => {
+      console.log("Error retrieving data from database:", err);
+      res.send(500);
+    });
+});
+
 module.exports = genresRouter;
